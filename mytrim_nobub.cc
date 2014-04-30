4cd45fa71fb7ae8f4ac761a89d1de88ec132469b
Topher Matthews <tophmatthews@gmail.com>
2014-04-30 11:43:45 -0700

    Changed file output to handle new ion energy calculations

--- mytrim_nobub.cc
+++ mytrim_nobub.cc
@@ -328,9 +328,9 @@ int main(int argc, char *argv[])
         if( pka->tag >= 0 )
         {
           hitNum++;
-          
           // calculate distance from center to final position
-          for( int i = 0; i < 3; i++ ) dif[i] = sample->c[i][pka->tag] - pka->pos[i];
+          for( int i = 0; i < 3; i++ )
+            dif[i] = sample->c[i][pka->tag] - pka->pos[i];
           fromcenter[1] = sqrt( v_dot( dif, dif ) );
           
           if( pka->pass > 0 && fromcenter[1] - r <= 10 )
@@ -342,12 +342,11 @@ int main(int argc, char *argv[])
             fprintf( hitFile, "%li\t%i\t%i\t%i\t", pka->ionId, pka->gen, pka->pass, pka->punch);
             fprintf( hitFile, "%.4f\t%.4f\t%.2f\t", fromcenter[0], fromcenter[1], pka->travel);
             fprintf( hitFile, "[ ");
-            for( int i = 0; i < pka->elist.size(); i++ )
-              fprintf( hitFile, "%.1f ", pka->elist.at(i));
-            fprintf(hitFile, "]\t[ ");
+            fprintf( hitFile, "%.2f\t%.2f\t%.2f", pka->Ehit, pka->Eout, pka->Eend);
+            fprintf( hitFile, " ]\t[ ");
             for( int i = 0; i < pka->famtree.size(); i++ )
               fprintf( hitFile, "%i ", pka->famtree.at(i));
-            fprintf( hitFile, "]\t" );
+            fprintf( hitFile, "] ");
             fprintf( hitFile,"%s", inbubble( sample->w, pka->pos, r)? "in " : "out");
             fprintf( hitFile, "\n");
           }
@@ -355,17 +354,18 @@ int main(int argc, char *argv[])
           if( pka->escapee )
           {
             escNum++;
+            if( fromcenter[1] < r )
+              backInNum++;
             if( save_escFile )
             {
               fprintf( escFile, "%li\t%i\t%i\t%i\t", pka->ionId, pka->gen, pka->pass, pka->punch);
               fprintf( escFile, "%.4f\t%.4f\t%.2f\t", fromcenter[0], fromcenter[1], pka->travel);
               fprintf( escFile, "[ ");
-              for( int i = 0; i < pka->elist.size(); i++ )
-                fprintf( escFile, "%.1f ", pka->elist.at(i));
-              fprintf(escFile, "]\t[ ");
+              fprintf( escFile, "%.2f\t%.2f\t%.2f", pka->Ehit, pka->Eout, pka->Eend);
+              fprintf(escFile, " ]\t[ ");
               for( int i = 0; i < pka->famtree.size(); i++ )
                 fprintf( escFile, "%i ", pka->famtree.at(i));
-              fprintf( escFile, "]\t" );
+              fprintf( escFile, "] ");
               fprintf( escFile,"%s", inbubble( sample->w, pka->pos, r)? "in " : "out");
               fprintf( escFile, "\n");
             }
